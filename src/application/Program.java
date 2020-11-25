0e9ab72cfc5f4ae92fd72cf15a29facc26d00852
felipeEAC <felipe.felipe_@live.com>
2020-11-25 20:10:19 -0300

    versao 2

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
-package apolication;
+package application;
+
+import java.util.Date;
 
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -9,6 +12,13 @@ public class Program {
 		
 		
 		Department obj = new Department (1 ,"Books");
+		
+		Seller seller  = new Seller(12 , "bob", "bob@gmail.com" , new Date(), 3000.0 , obj);
+		
+		System.out.println(seller);
+		
+		
+		
 		System.out.println(obj);
 	}
 
