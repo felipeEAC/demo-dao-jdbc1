380e584c6eeca8e3e58232cb29232b606036a094
felipeEAC <felipe.felipe_@live.com>
2020-11-25 19:30:16 -0300

    versao inicial

---
+++ src/apolication/Program.java
@@ -0,0 +1,15 @@
+package apolication;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated 
+		
+		
+		Department obj = new Department (1 ,"Books");
+		System.out.println(obj);
+	}
+
+}
