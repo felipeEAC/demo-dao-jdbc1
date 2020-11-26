b814843c3b2381e6e5939911f7d1d19906a5c443
felipeEAC <felipe.felipe_@live.com>
2020-11-26 03:29:37 -0300

    versao 10 - deletando dados

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -90,10 +90,27 @@ public class SellerDaoJDBC implements SellerDao{
 	}
 	@Override
 	public void deleteById(Integer id) {
-		// TODO Auto-generated method stub
-		
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement("DELETE FROM seller WHERE Id = ?");
+			
+			st.setInt(1, id);
+				
+		/*
+		int rows = st.executeUpdate();
+			if(rows == 0) {
+				throw new DbException("ID nao encontrado");
+			}  */
+			st.executeUpdate();
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+		}
 	}
-
+		
 	@Override
 	public Seller findById(Integer id) {
 		PreparedStatement st = null;
